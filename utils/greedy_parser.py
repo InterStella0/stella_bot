b10cf86842962e99c497787397615505bb93cc39
InterStella <sarahprog667@gmail.com>
2021-02-21 14:35:48 +0800

    Added new Greedy Parser that takes a custom separator

---
+++ utils/greedy_parser.py
@@ -0,0 +1,98 @@
+"""
+Copyright (C) by stella or something
+if you copy this but make your repository private, ur weird
+pls be nice to me if you do copy it that's all i want :pleading:
+"""
+import contextlib
+import discord
+from discord.ext import commands
+from discord.ext.commands import CommandError, ArgumentParsingError
+
+class GreedyParser(commands.Command):
+    """
+    Allows the ability to process Greedy converter result before it is passed into the command parameter.
+    Also allows for NewGreedy to have a new parsing method which is to split every ", " or any other passed
+    separator into NewGreedy class.
+    """
+    async def _transform_greedy_pos(self, ctx, param, required, converter):
+        result = await self.actual_greedy_parsing(ctx, param, required, converter)
+        if hasattr(converter, 'after_greedy'):
+            return await converter.after_greedy(ctx, result)
+        return result
+
+    async def actual_greedy_parsing(self, ctx, param, required, converter):
+        view = ctx.view
+        result = []
+        while not view.eof:
+            previous = view.index
+
+            view.skip_ws()
+            try:
+                if pos := view.get_parser(param.annotation):
+                    argument = view.get_arg_parser(pos)
+                else:
+                    argument = view.get_quoted_word()
+                value = await self.do_conversion(ctx, converter, argument, param)
+            except (CommandError, ArgumentParsingError):
+                view.index = previous
+                break
+            else:
+                result.append(value)
+
+        if not result and not required:
+            return param.default
+        return result
+
+    async def transform(self, ctx, param):
+        required = param.default is param.empty
+        converter = self._get_converter(param)
+        if isinstance(converter, commands.converter._Greedy):
+            if param.kind == param.POSITIONAL_OR_KEYWORD or param.kind == param.POSITIONAL_ONLY:
+                return await self._transform_greedy_pos(ctx, param, required, converter.converter)
+
+        return await super().transform(ctx, param)
+
+class WithCommaStringView(commands.view.StringView):
+    def __init__(self, view):
+        super().__init__(view.buffer)
+        self.old_view = view
+
+    def update_values(self):
+        self.__dict__.update({key: getattr(self.old_view, key) for key in ["previous", "index", "end"]})
+
+    def get_parser(self, converter):
+        if not hasattr(converter, "parsers"):
+            return
+        pos = 0
+        with contextlib.suppress(IndexError):
+            while not self.eof:
+                current = self.buffer[self.index + pos]
+                if current in converter.parsers:
+                    return pos
+                pos += 1
+
+    def get_arg_parser(self, end):
+        self.previous = self.index
+        offset = 0
+        PARSERSIZE = 1
+        # Undo if there is a space, to not capture it
+        while self.buffer[self.index + end - (1 + offset)].isspace():
+            offset += 1
+        result = self.buffer[self.index:self.index + (end - offset)]
+        self.index += end + PARSERSIZE
+        return result
+
+class _CustomGreedy(commands.converter._Greedy):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.parsers = {','}
+
+    def __call__(self, *parsers):
+        for x in parsers:
+            if len(x) != 1:
+                raise Exception("Separator must only be a single character.")
+
+        self.parsers |= set(parsers)
+        return self
+
+Greedy = _CustomGreedy()
