28f1622ce959408bea42a1ff435466277413ab55
InterStella0 <sarahprog667@gmail.com>
2021-11-13 01:09:48 +0800

    auto delete pending bots over 45 days

--- cogs/find_bot.py
+++ cogs/find_bot.py
@@ -27,7 +27,8 @@ from utils import flags as flg
 from utils.image_manipulation import get_majority_color, islight, create_bar, process_image
 from utils.new_converters import BotPrefixes, IsBot, BotCommands
 from utils.buttons import InteractionPages, PromptView
-from utils.useful import try_call, StellaEmbed, compile_array, search_prefixes, default_date, plural, realign, search_commands, StellaContext, aware_utc
+from utils.useful import try_call, StellaEmbed, compile_array, search_prefixes, default_date, plural, realign, \
+    search_commands, StellaContext, aware_utc, print_exception
 from utils.errors import NotInDatabase, BotNotFound
 from utils.decorators import is_discordpy, event_check, wait_ready, pages, listen_for_guilds
 from utils import greedy_parser
@@ -313,6 +314,7 @@ class FindBot(commands.Cog, name="Bots"):
         self.all_bot_prefixes = {}
         self.all_bot_commands = {}
         bot.loop.create_task(self.loading_all_prefixes())
+        bot.loop.create_task(self.task_handler())
 
     async def loading_all_prefixes(self) -> None:
         """Loads all unique prefix when it loads and set compiled_pref for C code."""
@@ -1451,6 +1453,41 @@ class FindBot(commands.Cog, name="Bots"):
 
         menu = InteractionPages(each_commands_list(bot.data), generate_page=True)
         await menu.start(ctx)
+
+    TASK_ID = 1001
+    EVERY_SEQUENCE = datetime.timedelta(days=45)
+
+    async def task_handler(self):
+        for count in itertools.count(1):
+            try:
+                print("Executing Sequence no", count)
+                await self.execute_task_at()
+            except Exception as e:
+                print_exception("Error while executing task: ", e)
+
+    async def execute_task_at(self):
+        data = await self.bot.pool_pg.fetchrow("SELECT * FROM bot_tasks WHERE task_id=$1", self.TASK_ID)
+        current = datetime.datetime.now(datetime.timezone.utc)
+        if not data:
+            query = "INSERT INTO bot_tasks VALUES ($1, $2, $3)"
+            next_time = current + self.EVERY_SEQUENCE
+            await self.bot.pool_pg.execute(query, self.TASK_ID, current, next_time)
+            await self.on_purge_old_pending()
+        else:
+            exec_time = data["next_execution"]
+            if exec_time <= current:
+                next_time = current + self.EVERY_SEQUENCE
+                query = "UPDATE bot_tasks SET last_execution=$1, next_execution=$2 WHERE task_id=$3"
+                await self.bot.pool_pg.execute(query, current, next_time, self.TASK_ID)
+                await self.on_purge_old_pending()
+            else:
+                next_time = exec_time
+
+        await discord.utils.sleep_until(next_time)
+
+    async def on_purge_old_pending(self):
+        far_time = datetime.datetime.utcnow() - self.EVERY_SEQUENCE
+        await self.bot.pool_pg.execute("DELETE FROM pending_bots WHERE requested_at <= $1", far_time)
 
 
 def setup(bot: StellaBot) -> None:
