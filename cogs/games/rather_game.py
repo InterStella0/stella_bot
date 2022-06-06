030c80bb29bdc531776d855db10e5533834dd7fd
Stella <sarahprog667@gmail.com>
2022-06-06 16:26:45 +0800

    added doc

--- cogs/games/rather_game.py
+++ cogs/games/rather_game.py
@@ -6,7 +6,10 @@ from utils.useful import StellaContext
 
 
 class RatherCog(BaseGameCog):
-    @commands.hybrid_command(aliases=["either.io", "either", "rathers", "rather-game", "rather-games"])
+    @commands.hybrid_command(
+        aliases=["either.io", "either", "rathers", "rather-game", "rather-games"],
+        help="Play would you rather. All data are fetched from either.io website."
+    )
     async def rather(self, ctx: StellaContext):
         handler = EitherIOView(self.http_rather)
         await handler.start(ctx)
